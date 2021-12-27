e9ddbccc7809a08a3d8b5618143e5e642142f79e
Ldeguzman9 <90979638+Ldeguzman9@users.noreply.github.com>
2021-12-27 11:51:47 -0800

    seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,30 +5,26 @@ VALUES
   (3, 'Operations'),
   (4, 'Finance'),
 
+INSERT INTO candidates (role_id, title, salary, department_id)
+VALUES
+  (1, 'Chief Executive Officer', '120,000', 1),
+  (2,'Assistant Manager', '85,000', 1),
+  (3, 'Marketing Director', '90,000', 2),
+  (4, 'Marketing Associate', '70,000', 2),
+  (5, 'Operations Manager', '90,000', 3),
+  (6, 'Operations Associate', '65,000', 3),
+  (7, 'Finance Director', '90,000', 4),
+  (8, 'Finance Associate', '65,000', 4),
 
-
--- INSERT INTO candidates (role_id, title, salary, department_id)
--- VALUES
---   ('Ronald', 'Firbank', 1, 1),
---   ('Virginia', 'Woolf', 1, 1),
---   ('Piers', 'Gaveston', 1, 0),
---   ('Charles', 'LeRoi', 2, 1),
---   ('Katherine', 'Mansfield', 2, 1),
---   ('Dora', 'Carrington', 3, 0),
---   ('Edward', 'Bellamy', 3, 0),
---   ('Montague', 'Summers', 3, 1),
---   ('Octavia', 'Butler', 3, 1),
---   ('Unica', 'Zurn', NULL, 1);
-
--- INSERT INTO employee (employee_id, first_name, last_name, role_id, manager_id)
--- VALUES
---   ('Ronald', 'Firbank', 1, 1),
---   ('Virginia', 'Woolf', 1, 1),
---   ('Piers', 'Gaveston', 1, 0),
---   ('Charles', 'LeRoi', 2, 1),
---   ('Katherine', 'Mansfield', 2, 1),
---   ('Dora', 'Carrington', 3, 0),
---   ('Edward', 'Bellamy', 3, 0),
---   ('Montague', 'Summers', 3, 1),
---   ('Octavia', 'Butler', 3, 1),
---   ('Unica', 'Zurn', NULL, 1);
+INSERT INTO employee (employee_id, first_name, last_name, role_id, manager_id)
+VALUES
+  (1, 'Ronald', 'Firbank', 1, NULL),
+  (2, 'Virginia', 'Woolf', 2, 1),
+  (3, 'Piers', 'Gaveston', 3, 1),
+  (4, 'Charles', 'LeRoi', 4, 3),
+  (5, 'Katherine', 'Mansfield', 4, 3),
+  (6, 'Dora', 'Carrington', 5, 1),
+  (7, 'Edward', 'Bellamy', 6, 5),
+  (8, 'Montague', 'Summers', 6, 5),
+  (9, 'Octavia', 'Butler', 7, 1),
+  (10, 'Unica', 'Zurn', 8, 7);
