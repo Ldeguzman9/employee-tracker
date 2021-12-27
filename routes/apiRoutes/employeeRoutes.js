5dc5c1752b109108d35c7044aa2eab61ebf3c9af
Ldeguzman9 <90979638+Ldeguzman9@users.noreply.github.com>
2021-12-27 13:44:15 -0800

    department and employee routes

---
+++ routes/apiRoutes/employeeRoutes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../../db/connection");
+const inputCheck = require("../../utils/inputCheck");
+
+// originally app.get('/api/employee')
+router.get("/employee", (req, res) => {
+  // internal logic remains the same
+});
+
+// originally app.get('/api/employee/:id')
+router.get("/employee/:id", (req, res) => {});
+
+// originally app.post('/api/employee')
+router.post("/employee", ({ body }, res) => {});
+
+// originally app.put('/api/employee/:id')
+router.put("/employee/:id", (req, res) => {});
+
+// originally app.delete('/api/employee/:id')
+router.delete("/employee/:id", (req, res) => {});
