dbb0406a7338118e3403be0a390873266edbe015
Ldeguzman9 <90979638+Ldeguzman9@users.noreply.github.com>
2021-12-27 11:37:57 -0800

    schema

--- db/schema.sql
+++ db/schema.sql
@@ -7,17 +7,17 @@ CREATE TABLE department (
   name VARCHAR(50) NOT NULL;
 );
 
--- CREATE TABLE role (
---   id INTEGER AUTO_INCREMENT PRIMARY KEY,
---   title VARCHAR(30) NOT NULL,
---   salary DECIMAL NOT NULL,
---   department_id INTEGER,
--- );
+CREATE TABLE role (
+  role_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INTEGER,
+);
 
--- CREATE TABLE employee (
---   id INTEGER AUTO_INCREMENT PRIMARY KEY,
---   first_name VARCHAR(30) NOT NULL,
---   last_name VARCHAR(30) NOT NULL,
---   role_id INTEGER,
---   manager_id INTEGER,
--- );
+CREATE TABLE employee (
+  employee_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER,
+  manager_id INTEGER,
+);
