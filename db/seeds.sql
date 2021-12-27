b9b45a68b7f3a32e5924f0de3bfe255ea1346b09
Ldeguzman9 <90979638+Ldeguzman9@users.noreply.github.com>
2021-12-27 11:16:47 -0800

    department seed

---
+++ db/seeds.sql
@@ -0,0 +1,34 @@
+INSERT INTO departments (department_id, name)
+VALUES
+  (1, 'General Management'),
+  (2, 'Marketing'),
+  (3, 'Operations'),
+  (4, 'Finance'),
+
+
+
+-- INSERT INTO candidates (role_id, title, salary, department_id)
+-- VALUES
+--   ('Ronald', 'Firbank', 1, 1),
+--   ('Virginia', 'Woolf', 1, 1),
+--   ('Piers', 'Gaveston', 1, 0),
+--   ('Charles', 'LeRoi', 2, 1),
+--   ('Katherine', 'Mansfield', 2, 1),
+--   ('Dora', 'Carrington', 3, 0),
+--   ('Edward', 'Bellamy', 3, 0),
+--   ('Montague', 'Summers', 3, 1),
+--   ('Octavia', 'Butler', 3, 1),
+--   ('Unica', 'Zurn', NULL, 1);
+
+-- INSERT INTO employee (employee_id, first_name, last_name, role_id, manager_id)
+-- VALUES
+--   ('Ronald', 'Firbank', 1, 1),
+--   ('Virginia', 'Woolf', 1, 1),
+--   ('Piers', 'Gaveston', 1, 0),
+--   ('Charles', 'LeRoi', 2, 1),
+--   ('Katherine', 'Mansfield', 2, 1),
+--   ('Dora', 'Carrington', 3, 0),
+--   ('Edward', 'Bellamy', 3, 0),
+--   ('Montague', 'Summers', 3, 1),
+--   ('Octavia', 'Butler', 3, 1),
+--   ('Unica', 'Zurn', NULL, 1);
