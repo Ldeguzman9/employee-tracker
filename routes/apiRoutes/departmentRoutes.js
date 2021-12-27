3620448b833e8b55a4e104d1a89e4463d6c4fbe4
Ldeguzman9 <90979638+Ldeguzman9@users.noreply.github.com>
2021-12-27 15:04:06 -0800

    routes added

--- routes/apiRoutes/departmentRoutes.js
+++ routes/apiRoutes/departmentRoutes.js
@@ -2,15 +2,15 @@ const express = require("express");
 const router = express.Router();
 const db = require("../../db/connection");
 
-// Get all parties
-router.get("/parties", (req, res) => {
+// Get all departments
+router.get("/department", (req, res) => {
   // internal logic remains the same
 });
 
-// Get single party
-router.get("/party/:id", (req, res) => {});
+// // Get single department
+// router.get("/department/:id", (req, res) => {});
 
-// Delete a party
-router.delete("/party/:id", (req, res) => {});
+// // Delete a department
+// router.delete("/deparment/:id", (req, res) => {});
 
 module.exports = router;
--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -5,4 +5,50 @@ router.use(require("./departmentRoutes"));
 router.use(require("./employeeRoutes"));
 router.use(require("./roleRoutes"));
 
+function promptQuestions() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "task",
+        message:
+          "Hello! Welcome to the company index. What would you like to do?",
+        choices: [
+          "View Departments",
+          "View Roles",
+          "View Employees",
+          "Add Department",
+          "Add Role",
+          "Add Employee",
+          "Update Employee Role",
+        ],
+      },
+    ])
+    .then(function (data) {
+      switch (data.task) {
+        case "View Departments":
+          displayDepartments();
+          break;
+        case "View Roles":
+          displayRoles();
+          break;
+        case "View Employees":
+          displayEmployees();
+          break;
+        case "Add Department":
+          addDepartment();
+          break;
+        case "Add Role":
+          addRole();
+          break;
+        case "Add Employee":
+          addEmployee();
+          break;
+        case "Update Employee Role":
+          updateEmployeeRole();
+          break;
+      }
+    });
+}
+
 module.exports = router;
